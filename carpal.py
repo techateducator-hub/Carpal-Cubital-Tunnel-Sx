6ab70fa72238c3195fc68199e4c6bbd8b0a20c18
techateducator-hub <techateducator@gmail.com>
2026-01-30 10:09:49 +1100

    Update carpal.py

--- carpal.py
+++ carpal.py
@@ -60,7 +60,7 @@ RUBRIC_LIST = [
 try:
     api_key = st.secrets["GEMINI_API_KEY"]
     genai.configure(api_key=api_key)
-    model = genai.GenerativeModel("gemini-1.5-flash")
+    model = genai.GenerativeModel("gemini-1.5-flash-latest")
 except Exception as e:
     st.error(f"Gemini setup error: {e}")
 
